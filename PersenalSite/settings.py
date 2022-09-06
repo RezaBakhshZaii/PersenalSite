3b82cecce70170d6b4d7ed5d8848a248e614304a
RezaBakhshZaii <rezabakh19@gmail.com>
2022-09-06 22:52:43 +0430

    add level

--- PersenalSite/settings.py
+++ PersenalSite/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'person/static/'
-
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, "person/static/")
+]
 STATIC_ROOT = os.path.join(BASE_DIR, "person/static/")
 
 # Default primary key field type
