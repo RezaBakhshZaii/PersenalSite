d2e409e4b2309453751b25e18654b6fee85b308f
RezaBakhshZaii <rezabakh19@gmail.com>
2022-09-15 18:15:38 +0430

    add level

--- PersenalSite/settings.py
+++ PersenalSite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t+mon9!f4jn^@d41ruyt4ueh&b40&9d@-eu=@%*7u&mmipbwpo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rezabakhshzaii.pythonanywhere.com']
 
 
 # Application definition
